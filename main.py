3c07924ecb9553aeb050d0fbe9949b6b302dd505
uc4151 <gemmalinda19@gmail.com>
2025-01-08 20:54:07 +0100

    user profile setup

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from forms import CreatePostForm, RegisterForm, LoginForm, PostForm, CommentForm, EmailForm
+from forms import CreatePostForm, RegisterForm, LoginForm, PostForm, CommentForm, EmailForm, ProfileForm
 from functools import wraps
 from flask import abort
 import hashlib
---
+++ static/js/sidebar.js
@@ -0,0 +1,9 @@
+function openProfileSidebar() {
+    console.log("Opening profile sidebar"); // Debugging statement
+    document.getElementById("profileSidebar").style.width = "250px"; // Adjust width as needed
+  }
+  
+  function closeProfileSidebar() {
+    console.log("Closing profile sidebar"); // Debugging statement
+    document.getElementById("profileSidebar").style.width = "0"; // Reset width to 0
+  }  
