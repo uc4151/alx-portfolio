78ece5671d13c2bd5def5747620085dc29772a3d
uc4151 <gemmalinda19@gmail.com>
2025-01-26 00:34:10 +0100

    detected redundant registration error 4

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ class TestConnection(db.Model):
 # Define User model
 class User(UserMixin, db.Model):
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
